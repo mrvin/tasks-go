120881d3a5306002f91213b459171265ace83da0
mr.Vin <v.v.vinogradovv@gmail.com>
2021-07-26 17:44:58 +0300

    002-get-film-info: add comment

--- 002-get-film-info/main.go
+++ 002-get-film-info/main.go
@@ -1,3 +1,8 @@
+// Problem from book 'The Go Programming Language. Alan A.A. Donovan,
+// Brian W. Kernighan'. Exercis 4.13: The JSON-based web service of the
+// Open Movie Database lets you search https://omdbapi.com/ for a movie by
+// name and download its poster image. Write a tool poster that downloads
+// the poster image for the movie named on the command line.
 package main
 
 import (
