5f82e54570b5cd38ca05ee6ad62997be74e34776
mr.Vin <v.v.vinogradovv@gmail.com>
2022-10-20 15:11:25 +0300

    006-imgstorage: add GetListImg to client

--- 006-imgstorage/client/go.mod
+++ 006-imgstorage/client/go.mod
@@ -3,12 +3,15 @@ module github.com/mrvin/tasks-go/006-imgstorage/client
 go 1.19
 
 require (
+	github.com/mrvin/tasks-go v0.0.0-20221020101055-7dab3704a7b9
+	google.golang.org/grpc v1.50.1
+)
+
+require (
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/mrvin/tasks-go v0.0.0-20221018131115-d66a2c244e07 // indirect
-	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
-	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
-	golang.org/x/text v0.3.3 // indirect
-	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
-	google.golang.org/grpc v1.50.1 // indirect
+	golang.org/x/net v0.1.0 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+	google.golang.org/genproto v0.0.0-20221018160656-63c7b68cfc55 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 )
--- 006-imgstorage/client/main.go
+++ 006-imgstorage/client/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"text/tabwriter"
 
 	"github.com/mrvin/tasks-go/006-imgstorage/internal/imgstorageapi"
 	"google.golang.org/grpc"
@@ -22,13 +23,14 @@ func main() {
 	var port int
 	var host string
 	var fileName string
-	var upload, download bool
+	var upload, download, list bool
 
 	flag.IntVar(&port, "port", 55555, "port")
 	flag.StringVar(&host, "host", "localhost", "host name")
 	flag.StringVar(&fileName, "name", "", "file name")
 	flag.BoolVar(&upload, "upload", false, "upload image")
 	flag.BoolVar(&download, "download", false, "download image")
+	flag.BoolVar(&list, "list", false, "image list")
 	flag.Parse()
 
 	conn, err := grpc.Dial(fmt.Sprintf("%s:%d", host, port), grpc.WithInsecure())
@@ -46,15 +48,18 @@ func main() {
 		return
 	}
 
-	if upload {
+	switch {
+	case upload:
 		if err := c.uploadImg(fileName); err != nil {
 			log.Printf("Upload image: %v", err)
 		}
-	}
-
-	if download {
+	case download:
 		if err := c.downloadImg(fileName); err != nil {
 			log.Printf("Download image: %v", err)
+		}
+	case list:
+		if err := c.getListImg(); err != nil {
+			log.Printf("Image list: %v", err)
 		}
 	}
 }
@@ -102,4 +107,26 @@ func (c *Client) downloadImg(fileName string) error {
 	}
 
 	return err
+}
+
+func (c *Client) getListImg() error {
+	req := &imgstorageapi.Null{}
+
+	pbListImg, err := c.client.GetListImg(ctx, req)
+	if err != nil {
+		return fmt.Errorf("get list image: %w", err)
+	}
+
+	const format = "%s\t%s\n"
+	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
+	fmt.Fprintf(tw, format, "File name", "Modified date")
+	fmt.Fprintf(tw, format, "---------", "-------------")
+
+	for _, infImg := range pbListImg.InfImg {
+		fmt.Fprintf(tw, format, infImg.Name, infImg.ModifiedAt.AsTime().Format("2 Jan 2006 15:04"))
+	}
+
+	tw.Flush()
+
+	return nil
 }
