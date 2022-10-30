628991cd65d4514f161ac58d139e5bfd6a8bb326
mr.Vin <v.v.vinogradovv@gmail.com>
2022-10-30 18:04:45 +0300

    006-imgstorage: limited number of connections from one ip

--- 006-imgstorage/server/grpcserver/server.go
+++ 006-imgstorage/server/grpcserver/server.go
@@ -10,9 +10,13 @@ import (
 	"net"
 	"os"
 	"path/filepath"
+	"sync"
+
+	"golang.org/x/sync/semaphore"
 
 	"github.com/mrvin/tasks-go/006-imgstorage/internal/imgstorageapi"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/peer"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
@@ -25,27 +29,34 @@ const numGoroutinList = 100
 
 const chunkSize = 64 * 1024 // 64 KiB
 
+var ErrExceededUploadDownload = errors.New("upload and download limit exceeded for client")
+var ErrExceededList = errors.New("get list limit exceeded for client")
+
 type Config struct {
 	Host string `yaml:"host"`
 	Port int    `yaml:"port"`
 	Dir  string `yaml:"dir"`
 }
 
+type clientSema struct {
+	semaUploadDownload *semaphore.Weighted
+	semaList           *semaphore.Weighted
+}
+
 type Server struct {
-	serv               *grpc.Server
-	ln                 net.Listener
-	addr               string
-	dir                string
-	semaUploadDownload chan struct{}
-	semaList           chan struct{}
+	serv         *grpc.Server
+	ln           net.Listener
+	addr         string
+	dir          string
+	mClientSema  map[string]clientSema
+	muClientSema sync.RWMutex
 }
 
 func New(conf *Config) (*Server, error) {
 	var server Server
 
 	server.dir = conf.Dir
-	server.semaUploadDownload = make(chan struct{}, numGoroutinUploadDownload)
-	server.semaList = make(chan struct{}, numGoroutinList)
+	server.mClientSema = make(map[string]clientSema)
 
 	var err error
 	server.addr = fmt.Sprintf("%s:%d", conf.Host, conf.Port)
@@ -75,8 +86,20 @@ func (s *Server) Stop() {
 }
 
 func (s *Server) UploadImage(stream imgstorageapi.ImgStorage_UploadImageServer) error { //nolint: nosnakecase
-	s.semaUploadDownload <- struct{}{}        // acquire token
-	defer func() { <-s.semaUploadDownload }() // release token
+	ctx := stream.Context()
+
+	clientIP, err := login(ctx, s.mClientSema, &s.muClientSema)
+	if err != nil {
+		return fmt.Errorf("login: %w", err)
+	}
+
+	s.muClientSema.RLock()
+	if !s.mClientSema[clientIP].semaUploadDownload.TryAcquire(1) {
+		s.muClientSema.RUnlock()
+		return ErrExceededUploadDownload
+	}
+	s.muClientSema.RUnlock()
+	defer func() { s.mClientSema[clientIP].semaUploadDownload.Release(1) }()
 
 	req, err := stream.Recv()
 	if err != nil {
@@ -93,8 +116,10 @@ func (s *Server) UploadImage(stream imgstorageapi.ImgStorage_UploadImageServer) 
 
 	imageSize := 0
 	for {
-		if err := stream.Context().Err(); err != nil {
-			return fmt.Errorf("termination due to context: %w", err)
+		if ctx.Err() != nil {
+			err := fmt.Errorf("termination due to context: %w", ctx.Err())
+			log.Print(err)
+			return err
 		}
 		req, err := stream.Recv()
 		if errors.Is(err, io.EOF) {
@@ -127,8 +152,20 @@ func (s *Server) UploadImage(stream imgstorageapi.ImgStorage_UploadImageServer) 
 }
 
 func (s *Server) DownloadImage(nameImg *imgstorageapi.DownloadImageRequest, stream imgstorageapi.ImgStorage_DownloadImageServer) error { //nolint: nosnakecase
-	s.semaUploadDownload <- struct{}{}        // acquire token
-	defer func() { <-s.semaUploadDownload }() // release token
+	ctx := stream.Context()
+
+	clientIP, err := login(ctx, s.mClientSema, &s.muClientSema)
+	if err != nil {
+		return fmt.Errorf("login: %w", err)
+	}
+
+	s.muClientSema.RLock()
+	if !s.mClientSema[clientIP].semaUploadDownload.TryAcquire(1) {
+		s.muClientSema.RUnlock()
+		return ErrExceededUploadDownload
+	}
+	s.muClientSema.RUnlock()
+	defer func() { s.mClientSema[clientIP].semaUploadDownload.Release(1) }()
 
 	name := nameImg.GetName()
 
@@ -143,8 +180,8 @@ func (s *Server) DownloadImage(nameImg *imgstorageapi.DownloadImageRequest, stre
 
 	imageSize := 0
 	for {
-		if err := stream.Context().Err(); err != nil {
-			return fmt.Errorf("termination due to context: %w", err)
+		if ctx.Err() != nil {
+			return fmt.Errorf("termination due to context: %w", ctx.Err())
 		}
 		size, err := reader.Read(buffer)
 		if err == io.EOF {
@@ -170,8 +207,18 @@ func (s *Server) DownloadImage(nameImg *imgstorageapi.DownloadImageRequest, stre
 }
 
 func (s *Server) GetListImage(ctx context.Context, _ *emptypb.Empty) (*imgstorageapi.GetListImageResponse, error) {
-	s.semaList <- struct{}{}        // acquire token
-	defer func() { <-s.semaList }() // release token
+	clientIP, err := login(ctx, s.mClientSema, &s.muClientSema)
+	if err != nil {
+		return nil, fmt.Errorf("login: %w", err)
+	}
+
+	s.muClientSema.RLock()
+	if !s.mClientSema[clientIP].semaList.TryAcquire(1) {
+		s.muClientSema.RUnlock()
+		return nil, ErrExceededList
+	}
+	s.muClientSema.RUnlock()
+	defer func() { s.mClientSema[clientIP].semaList.Release(1) }()
 
 	entries, err := os.ReadDir(s.dir)
 	if err != nil {
@@ -192,4 +239,33 @@ func (s *Server) GetListImage(ctx context.Context, _ *emptypb.Empty) (*imgstorag
 	log.Println("Get image list")
 
 	return &imgstorageapi.GetListImageResponse{ImageInfo: listImg}, nil
+}
+
+func login(ctx context.Context, mClientSema map[string]clientSema, mu *sync.RWMutex) (string, error) {
+	clientIP, err := getClientIP(ctx)
+	if err != nil {
+		return "", fmt.Errorf("get client ip: %w", err)
+	}
+
+	mu.Lock()
+	if _, ok := mClientSema[clientIP]; !ok {
+		mClientSema[clientIP] = clientSema{semaphore.NewWeighted(numGoroutinUploadDownload), semaphore.NewWeighted(numGoroutinList)}
+	}
+	mu.Unlock()
+
+	return clientIP, nil
+}
+
+func getClientIP(ctx context.Context) (string, error) {
+	p, ok := peer.FromContext(ctx)
+	if !ok {
+		return "", fmt.Errorf("cant get perr")
+	}
+
+	ip, _, err := net.SplitHostPort(p.Addr.String())
+	if err != nil {
+		return "", fmt.Errorf("split host port: %w", err)
+	}
+
+	return ip, nil
 }
