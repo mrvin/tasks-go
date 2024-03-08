e1ceab209e78679d76c526dded58d2241bfd23ba
mr.Vin <v.v.vinogradovv@gmail.com>
2024-03-08 16:44:11 +0300

    books: add case 'List of books' to books-client

--- books/cmd/books-client/main.go
+++ books/cmd/books-client/main.go
@@ -7,9 +7,11 @@ import (
 	"log"
 	"os"
 	"strings"
+	"text/tabwriter"
 
 	"github.com/mrvin/tasks-go/books/internal/booksapi"
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 func main() {
@@ -21,7 +23,7 @@ func main() {
 	client := booksapi.NewBookServiceClient(conn)
 exit:
 	for {
-		fmt.Printf("0 - Exit\n1 - Save book\n2 - Search by author\n3 - Search by title\n")
+		fmt.Printf("0 - Exit\n1 - Save book\n2 - Search by title\n3 - Search by author\n4 - List of books\n")
 		reader := bufio.NewReader(os.Stdin)
 		input, _ := reader.ReadString('\n')
 		switch []byte(input)[0] {
@@ -31,11 +33,12 @@ exit:
 		case '1':
 			fmt.Printf("Title:")
 			title, _ := reader.ReadString('\n')
+			title = strings.TrimSuffix(title, "\n")
 			fmt.Printf("Authors:")
 			authors, _ := reader.ReadString('\n')
 
-			req := &booksapi.CreateBookRequest{
-				Title:   strings.TrimSuffix(title, "\n"),
+			req := &booksapi.Book{
+				Title:   title,
 				Authors: strings.Split(strings.TrimSuffix(authors, "\n"), ", "),
 			}
 
@@ -45,39 +48,64 @@ exit:
 				log.Fatalf("Save book: %v", err)
 			}
 		case '2':
-			fmt.Printf("Author:")
-			author, _ := reader.ReadString('\n')
-
-			req := &booksapi.GetBooksByAuthorRequest{
-				Author: strings.TrimSuffix(author, "\n"),
-			}
-
-			ctx, cancel := context.WithCancel(context.Background())
-			defer cancel()
-			res, err := client.GetBooksByAuthor(ctx, req)
-			if err != nil {
-				log.Fatalf("Search by author: %v", err)
-			}
-
-			fmt.Printf("Titles: %v\n", res.Titles)
-		case '3':
 			fmt.Printf("Title:")
 			title, _ := reader.ReadString('\n')
+			title = strings.TrimSuffix(title, "\n")
 
-			req := &booksapi.GetAuthorsByTitleRequest{
-				Title: strings.TrimSuffix(title, "\n"),
+			req := &booksapi.Title{
+				Title: title,
 			}
 
 			ctx, cancel := context.WithCancel(context.Background())
 			defer cancel()
 
-			res, err := client.GetAuthorsByTitle(ctx, req)
+			res, err := client.GetBookByTitle(ctx, req)
 			if err != nil {
 				log.Fatalf("Search by title: %v", err)
 			}
 
-			fmt.Printf("Authors: %v\n", res.Authors)
+			printListBooks("Book with title "+title, []*booksapi.Book{res})
+		case '3':
+			fmt.Printf("Author:")
+			author, _ := reader.ReadString('\n')
+			author = strings.TrimSuffix(author, "\n")
+
+			req := &booksapi.Author{
+				Author: author,
+			}
+
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+			res, err := client.ListBooksByAuthor(ctx, req)
+			if err != nil {
+				log.Fatalf("Search by author: %v", err)
+			}
+
+			printListBooks("List of books by author "+author, res.Books)
+		case '4':
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+			res, err := client.ListAllBooks(ctx, &emptypb.Empty{})
+			if err != nil {
+				log.Fatalf("List of books: %v", err)
+			}
+
+			printListBooks("List of books", res.Books)
 		default:
 		}
 	}
+}
+
+func printListBooks(header string, slBooks []*booksapi.Book) {
+	fmt.Println(header)
+	const formatHeader = "%s\t%s\t%s\n"
+	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
+	fmt.Fprintf(tw, formatHeader, "Number", "Title", "Authors")
+	fmt.Fprintf(tw, formatHeader, "------", "-----", "------")
+
+	for i, book := range slBooks {
+		fmt.Fprintf(tw, "%d\t%s\t%s\n", i+1, book.Title, book.Authors)
+	}
+
+	tw.Flush()
 }
