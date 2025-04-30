34769744ef12ff55fc4189132f0b10e87da0a45d
mr.Vin <v.v.vinogradovv@gmail.com>
2025-04-30 14:37:55 +0300

    persons: loading config from .env files

--- persons/cmd/persons-server/main.go
+++ persons/cmd/persons-server/main.go
@@ -2,50 +2,36 @@ package main
 
 import (
 	"context"
-	"errors"
-	"flag"
-	stdlog "log"
+	"log"
 	"log/slog"
-	"net/http"
-	"sync"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/mrvin/tasks-go/persons/internal/config"
 	"github.com/mrvin/tasks-go/persons/internal/httpserver"
 	"github.com/mrvin/tasks-go/persons/internal/logger"
-
 	sqlstorage "github.com/mrvin/tasks-go/persons/internal/storage/sql"
 )
 
-type Config struct {
-	DB     sqlstorage.Conf `yaml:"db"`
-	HTTP   httpserver.Conf `yaml:"http"`
-	Logger logger.Conf     `yaml:"logger"`
-}
-
-var ctx = context.Background()
-
 func main() {
-	configFile := flag.String("config", "/etc/persons/persons.yml", "path to configuration file")
-	flag.Parse()
-
-	var conf Config
-	if err := config.Parse(*configFile, &conf); err != nil {
-		stdlog.Printf("Parse config: %v", err)
-		return
-	}
+	// init config
+	var conf config.Config
+	conf.LoadFromEnv()
 
 	logFile, err := logger.Init(&conf.Logger)
 	if err != nil {
-		stdlog.Printf("Init logger: %v\n", err)
+		log.Printf("Init logger: %v\n", err)
 		return
-	} else {
-		slog.Info("Init logger", slog.String("Logging level", conf.Logger.Level))
-		defer func() {
-			if err := logFile.Close(); err != nil {
-				slog.Error("Close log file: " + err.Error())
-			}
-		}()
 	}
+	slog.Info("Init logger", slog.String("Logging level", conf.Logger.Level))
+	defer func() {
+		if err := logFile.Close(); err != nil {
+			slog.Error("Close log file: " + err.Error())
+		}
+	}()
+
+	ctx := context.Background()
 	slog.Info("Storage in sql database")
 	storage, err := sqlstorage.New(ctx, &conf.DB)
 	if err != nil {
@@ -53,26 +39,23 @@ func main() {
 		return
 	}
 	slog.Info("Connected to database")
-
-	serverHTTP := httpserver.New(&conf.HTTP, storage)
-
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-
-		var err error
-		err = serverHTTP.Start()
-		if !errors.Is(err, http.ErrServerClosed) {
-			slog.Error("Failed to start http server: " + err.Error())
-			return
+	defer func() {
+		if err := storage.Close(); err != nil {
+			slog.Error("Failed to close storage: " + err.Error())
+		} else {
+			slog.Info("Closing the database connection")
 		}
 	}()
-	wg.Wait()
 
-	if err := storage.Close(); err != nil {
-		slog.Error("Failed to close storage: " + err.Error())
-	} else {
-		slog.Info("Closing the database connection")
-	}
+	server := httpserver.New(&conf.HTTP, storage)
+
+	ctx, cancel := signal.NotifyContext(
+		ctx,
+		os.Interrupt,    // SIGINT, (Control-C)
+		syscall.SIGTERM, // systemd
+		syscall.SIGQUIT,
+	)
+	defer cancel()
+
+	server.Run(ctx)
 }
--- persons/internal/config/config.go
+++ persons/internal/config/config.go
@@ -1,21 +1,62 @@
 package config
 
 import (
-	"fmt"
+	"log/slog"
 	"os"
 
-	"gopkg.in/yaml.v3"
+	"github.com/mrvin/tasks-go/persons/internal/httpserver"
+	"github.com/mrvin/tasks-go/persons/internal/logger"
+	sqlstorage "github.com/mrvin/tasks-go/persons/internal/storage/sql"
 )
 
-func Parse(configPath string, conf interface{}) error {
-	configYml, err := os.ReadFile(configPath)
-	if err != nil {
-		return fmt.Errorf("reading error: %w", err)
+type Config struct {
+	HTTP   httpserver.Conf
+	DB     sqlstorage.Conf
+	Logger logger.Conf
+}
+
+// LoadFromEnv will load configuration solely from the environment.
+func (c *Config) LoadFromEnv() {
+	if addr := os.Getenv("API_HTTP_ADDR"); addr != "" {
+		c.HTTP.Addr = addr
+	} else {
+		slog.Warn("Empty http addr")
 	}
 
-	if err := yaml.Unmarshal(configYml, conf); err != nil {
-		return fmt.Errorf("can't unmarshal %s: %w", configPath, err)
+	if host := os.Getenv("POSTGRES_HOST"); host != "" {
+		c.DB.Host = host
+	} else {
+		slog.Warn("Empty postgres host")
+	}
+	if port := os.Getenv("POSTGRES_PORT"); port != "" {
+		c.DB.Port = port
+	} else {
+		slog.Warn("Empty postgres port")
+	}
+	if user := os.Getenv("POSTGRES_USER"); user != "" {
+		c.DB.User = user
+	} else {
+		slog.Warn("Empty postgres user")
+	}
+	if password := os.Getenv("POSTGRES_PASSWORD"); password != "" {
+		c.DB.Password = password
+	} else {
+		slog.Warn("Empty postgres password")
+	}
+	if name := os.Getenv("POSTGRES_DB"); name != "" {
+		c.DB.Name = name
+	} else {
+		slog.Warn("Empty postgres db name")
 	}
 
-	return nil
+	if logFilePath := os.Getenv("LOGGER_FILEPATH"); logFilePath != "" {
+		c.Logger.FilePath = logFilePath
+	} else {
+		slog.Warn("Empty log file path")
+	}
+	if logLevel := os.Getenv("LOGGER_LEVEL"); logLevel != "" {
+		c.Logger.Level = logLevel
+	} else {
+		slog.Warn("Empty log level")
+	}
 }
