4d94e0ee42e4c6b53fe5de9d83ede981e4537068
mr.Vin <v.v.vinogradovv@gmail.com>
2025-05-07 17:56:33 +0300

    persons: add enumerated type for field 'gender'

--- persons/migrations/000001_init_schema.up.sql
+++ persons/migrations/000001_init_schema.up.sql
@@ -1,9 +1,11 @@
+CREATE TYPE gender_type AS ENUM ('male', 'female');
+
 CREATE TABLE IF NOT EXISTS persons (
 	id SERIAL NOT NULL UNIQUE PRIMARY KEY,
 	name TEXT,
 	surname TEXT,
 	patronymic TEXT,
 	age INT,
-	gender TEXT,
+	gender gender_type,
 	country_id TEXT
 );
